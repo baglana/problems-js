2d95a23d78bec56b6297ab2367fc808e90788313
baglana <102351104+baglana@users.noreply.github.com>
2023-09-11 19:02:49 +0600

    ex2/p9: fix sort() func

--- exercise2/problem9/index.js
+++ exercise2/problem9/index.js
@@ -1,5 +1,5 @@
 function duplicateNums(nums) {
-  nums.sort();
+  nums.sort((a, b) => a - b);
   let duplicates = [];
   for (let i = 1; i < nums.length; i++) {
     if (nums[i] === nums[i - 1] && !duplicates.includes(nums[i])) {
@@ -11,8 +11,12 @@ function duplicateNums(nums) {
 
 module.exports = duplicateNums;
 
+// console.log(duplicateNums([100, 59, 12, 13, 54, 76, 100, 14, 12]));
+
+/*
 console.log(duplicateNums([1, 2, 3, 4, 3, 5, 6])) // [3]
 
 console.log(duplicateNums([81, 72, 43, 72, 81, 99, 99, 100, 12, 54])) // [72, 81, 99]
 
 console.log(duplicateNums([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // []
+*/
