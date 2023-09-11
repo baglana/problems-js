9693514c0cda46204d926884ca73fe16a0b2acca
baglana <102351104+baglana@users.noreply.github.com>
2023-09-11 19:17:39 +0600

    Solve exercise2/problem10

--- exercise2/problem10/index.js
+++ exercise2/problem10/index.js
@@ -1,5 +1,10 @@
-function getStudentsWithNamesAndTopNotes() {
-  // Your code
+function getStudentsWithNamesAndTopNotes(students) {
+  let studentsWithNamesAndTopNotes = [...students];
+  for (let student of studentsWithNamesAndTopNotes) {
+    student.topNote = student.notes.reduce((a, b) => Math.max(a, b), 0);
+    delete student.notes;
+  }
+  return studentsWithNamesAndTopNotes;
 }
 
 module.exports = getStudentsWithNamesAndTopNotes;
