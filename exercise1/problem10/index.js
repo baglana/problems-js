90553c6674e2ec3a3aee6ff9a7469d25089943ac
baglana <102351104+baglana@users.noreply.github.com>
2023-09-14 21:36:26 +0600

    ex1/p10: move char changing logic to moveChar() and handle edge cases

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,19 +1,41 @@
 function move(str) {
   let arr = str.split('');
   for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i);
-    charCode++;
-    arr.splice(i, 1, String.fromCharCode(charCode));
-    /*
-        let codePoint = str.codePointAt(i);
-        codePoint++;
-        arr.splice(i, 1, String.fromCodePoint(codePoint));
-    */
+    moveChar(i, str, arr);
   }
   return arr.join('');
 }
 
+function moveChar(i, str, arr) {
+  const charMap = {
+    z: 'a',
+    Z: 'A'
+  }
+  const oldChar = str.charAt(i);
+  let newChar;
+  switch (oldChar) {
+    case 'z':
+    case 'Z':
+      newChar = charMap[oldChar];
+      break;
+    default:
+      let charCode = str.charCodeAt(i);
+      charCode++;
+      newChar = String.fromCharCode(charCode);
+/*
+      // Using codePoint string methods
+      let codePoint = str.codePointAt(i);
+      codePoint++;
+      newChar = String.fromCodePoint(codePoint);
+*/
+      break;
+  }
+  arr.splice(i, 1, newChar);
+}
+
 module.exports = move;
+
+console.log(move("zZ")); // "aA"
 
 console.log(move("hello")) // "ifmmp"
 
