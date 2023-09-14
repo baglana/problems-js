b88cca843d8158cd317af22866f3c1efd6e61cca
baglana <102351104+baglana@users.noreply.github.com>
2023-09-14 21:12:10 +0600

    ex1/p10: substitute codePoint string methods to charCode

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
 function move(str) {
   let arr = str.split('');
   for (let i = 0; i < str.length; i++) {
-    let codePoint = str.codePointAt(i);
-    codePoint++;
-    arr.splice(i, 1, String.fromCodePoint(codePoint));
+    let charCode = str.charCodeAt(i);
+    charCode++;
+    arr.splice(i, 1, String.fromCharCode(charCode));
+    /*
+        let codePoint = str.codePointAt(i);
+        codePoint++;
+        arr.splice(i, 1, String.fromCodePoint(codePoint));
+    */
   }
   return arr.join('');
 }
 
 module.exports = move;
 
-/*
 console.log(move("hello")) // "ifmmp"
 
 console.log(move("bye")) // "czf"
 
 console.log(move("welcome")) // "xfmdpnf"
-*/
-=======
-function move() {
-  // Your code
-}
-
-module.exports = move;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
