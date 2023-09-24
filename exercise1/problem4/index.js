3959fc1f4c81d2ae73b72fca83d64adff6c8b3dd
baglana <102351104+baglana@users.noreply.github.com>
2023-09-24 23:33:01 +0600

    ex1/p4: refactor and remove console.logs

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
 function makeTitleCase(string) {
   const tokens = string.split(' ');
   for (let i in tokens) {
-    tokens[i] = tokens[i].substring(0, 1).toUpperCase()
-      + tokens[i].substring(1);
+    tokens[i] = capitalizeFirstCharOf(tokens[i]);
   }
   return tokens.join(' ');
 }
 
-module.exports = makeTitleCase;
-
-/*
-console.log(makeTitleCase("This is a title")) // "This Is A Title"
-
-console.log(makeTitleCase("capitalize every word")) // "Capitalize Every Word"
-
-console.log(makeTitleCase("I Like Pizza")) // "I Like Pizza"
-
-console.log(makeTitleCase("PIZZA PIZZA PIZZA")) // "PIZZA PIZZA PIZZA"
-*/
-=======
-function makeTitleCase() {
-  // Your code
+function capitalizeFirstCharOf(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 module.exports = makeTitleCase;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
