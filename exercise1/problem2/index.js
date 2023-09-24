17d306221691e35c64a56f30ac22fe9870093b27
baglana <102351104+baglana@users.noreply.github.com>
2023-09-24 22:16:33 +0600

    ex1/p2: fix formatting

--- exercise1/problem2/index.js
+++ exercise1/problem2/index.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 function intWithinBounds(n, lower, upper) {
-  if (!Number.isInteger(n))
+  if (!Number.isInteger(n)) {
     return false;
-  else {
-    return n >= lower && n < upper;
   }
+  return lower < n && n < upper;
 }
 
 module.exports = intWithinBounds;
 
-/*
 console.log(intWithinBounds(3, 1, 9)) // true
 
 console.log(intWithinBounds(6, 1, 6)) // false
 
 console.log(intWithinBounds(4.5, 3, 8)) // false
-*/
-=======
-function intWithinBounds() {
-  // Your code
-}
-
-module.exports = intWithinBounds;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
