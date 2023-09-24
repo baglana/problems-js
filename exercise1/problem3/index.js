4c2dafb08b373bf0b0a521a82797d97b9fce2dba
baglana <102351104+baglana@users.noreply.github.com>
2023-09-24 22:23:09 +0600

    ex1/p3: fix formatting and remove console.logs

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
 function pentagonal(num) {
   return (5 * num ** 2 - 5 * num + 2) / 2;
 }
 
 module.exports = pentagonal;
-
-/*
-console.log(pentagonal(1)) // 1
-
-console.log(pentagonal(2)) // 6
-
-console.log(pentagonal(3)) // 16
-
-console.log(pentagonal(8)) // 141
-*/
-=======
-function pentagonal() {
-  // Your code
-}
-
-module.exports = pentagonal;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
