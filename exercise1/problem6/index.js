113da31981606b4e260fedb0be83f2a25adbcc90
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 13:02:06 +0600

    refactor(ex1/p6): fix formatting, add func for pattern check, remove console.logs

--- exercise1/problem6/index.js
+++ exercise1/problem6/index.js
@@ -1,31 +1,19 @@
-<<<<<<< HEAD
 function insertWhitespace(string) {
-  const upperCaseRegex = /[A-Z]/;
-  const lowerCaseRegex = /[a-z]/;
-
-  let arr = string.split('');
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i].match(lowerCaseRegex) && arr[i + 1].match(upperCaseRegex)) {
-      arr.splice(i + 1, 0, ' ');
+  let chars = string.split('');
+  for (let i = 0; i < chars.length - 1; i++) {
+    if (checkPattern(chars[i], chars[i + 1])) {
+      chars.splice(i + 1, 0, ' ');
     }
   }
 
-  return arr.join('');
+  return chars.join('');
+}
+
+function checkPattern(char1, char2) {
+  const upperCaseRegex = /[A-Z]/;
+  const lowerCaseRegex = /[a-z]/;
+  return lowerCaseRegex.test(char1)
+      && upperCaseRegex.test(char2);
 }
 
 module.exports = insertWhitespace;
-
-/*
-console.log(insertWhitespace("SheWalksToTheBeach")) // "She Walks To The Beach"
-
-console.log(insertWhitespace("MarvinTalksTooMuch")) // "Marvin Talks Too Much"
-
-console.log(insertWhitespace("TheGreatestUpsetInHistory")) // "The Greatest Upset In History"
-*/
-=======
-function insertWhitespace() {
-  // Your code
-}
-
-module.exports = insertWhitespace;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
