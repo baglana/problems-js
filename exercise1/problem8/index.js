f2f2b6b92b01038737612afa85cf0b429f021c6f
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 16:34:36 +0600

    refactor(ex1/p8): fix formatting, replace if with switch, remove console.logs

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 function longestTime(h, m, s) {
   hSeconds = h * 60 * 60;
   mSeconds = m * 60;
   const maxSeconds = Math.max(hSeconds, mSeconds, s);
-  if (maxSeconds === s) return s;
-  else if (maxSeconds === mSeconds) return m;
-  else return h;
+
+  switch (maxSeconds) {
+    case s:
+      return s;
+
+    case mSeconds:
+      return m;
+
+    default:
+      return h;
+  }
 }
 
 module.exports = longestTime;
-
-/*
-console.log(longestTime(1, 59, 3598)) // 1
-
-console.log(longestTime(2, 300, 15000)) // 300
-
-console.log(longestTime(15, 955, 59400)) // 59400
-*/
-=======
-function longestTime() {
-  // Your code
-}
-
-module.exports = longestTime;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
