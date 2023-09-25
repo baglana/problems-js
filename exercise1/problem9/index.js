7ad6c4603e33f271309e521fb57afb7ead72a02b
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 17:00:58 +0600

    refactor(ex1/p9): fix formatting, rename variable, change loop condition

--- exercise1/problem9/index.js
+++ exercise1/problem9/index.js
@@ -1,36 +1,12 @@
-<<<<<<< HEAD
 function isDisarium(n) {
-  const N = n;
+  const num = n;
   const len = n.toString().length;
   let sum = 0;
-  for (let pos = len; n !== 0; pos--) {
+  for (let pos = len; n > 0; pos--) {
     sum += Math.pow(n % 10, pos);
     n = Math.floor(n / 10);
   }
-  return sum === N;
+  return sum === num;
 }
 
 module.exports = isDisarium;
-
-/*
-console.log(isDisarium(75)) // false
-// 7^1 + 5^2 = 7 + 25 = 32
-
-console.log(isDisarium(135)) // true
-// 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
-
-console.log(isDisarium(544)) // false
-
-console.log(isDisarium(518)) // true
-
-console.log(isDisarium(8)) // true
-
-console.log(isDisarium(466)) // false
-*/
-=======
-function isDisarium() {
-  // Your code
-}
-
-module.exports = isDisarium;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
