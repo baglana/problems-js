2abd74c9cc6f4e11e20ca05897b96d7a21d6b36b
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 18:02:37 +0600

    refactor(ex1/p10): delete original solution

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -19,41 +19,4 @@ function getNextChar(char) {
   return String.fromCharCode(charCode);
 }
 
-/*
-function move(str) {
-  const chars = str.split('');
-  for (let i = 0; i < str.length; i++) {
-    moveChar(i, str, chars);
-  }
-  return chars.join('');
-}
-*/
-
-function moveChar(i, str, arr) {
-  const charDict = {
-    z: 'a',
-    Z: 'A'
-  }
-  const oldChar = str.charAt(i);
-  let newChar;
-  switch (oldChar) {
-    case 'z':
-    case 'Z':
-      newChar = charDict[oldChar];
-      break;
-    default:
-      let charCode = str.charCodeAt(i);
-      charCode++;
-      newChar = String.fromCharCode(charCode);
-/*
-      // Using codePoint string methods
-      let codePoint = str.codePointAt(i);
-      codePoint++;
-      newChar = String.fromCodePoint(codePoint);
-*/
-      break;
-  }
-  arr.splice(i, 1, newChar);
-}
-
 module.exports = move;
