126c3f3ab1238b116c29ae03ec17d4d1b091e4fb
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 16:23:22 +0600

    Fix formatting and add alternative solutions

--- exercise1/problem7/index.js
+++ exercise1/problem7/index.js
@@ -1,25 +1,32 @@
-<<<<<<< HEAD
+function isPrimitive(val) {
+  return !(val instanceof Object);
+}
+
+/*
 function isPrimitive(val) {
   return val !== Object(val);
 }
-
-module.exports = isPrimitive;
+*/
 
 /*
-console.log(isPrimitive(function add(x, y) {
-  return x + y
-})) // true
-
-console.log(isPrimitive(new RegExp('^[a-zA-Z0-9]+$', 'g'))) // true
-
-console.log(isPrimitive(null)) // false
-
-console.log(isPrimitive("")) // false
-*/
-=======
-function isPrimitive() {
-  // Your code
+function isPrimitive(val) {
+  const primitives = [
+    'boolean', 'number', 'bigint',
+    'string', 'undefined', 'symbol'
+  ];
+  return val === null
+  || primitives.includes(typeof val);
 }
+*/
+
+/*
+function isPrimitive(val) {
+  const nonPrimitives = [
+    'object', 'function'
+  ];
+  return val === null
+  || !(nonPrimitives.includes(typeof val));
+}
+*/
 
 module.exports = isPrimitive;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
