ff9a5d050c0717ccbd180b217147770f8b144fb6
baglana <102351104+baglana@users.noreply.github.com>
2023-09-25 12:34:32 +0600

    refactor(ex1/p5): fix formatting and remove console.logs

--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
 function removeABC(string) {
-  const regex = /[a-c]/g;
-  if (!string.match(regex))
+  const regex = /[abc]/g;
+  if (!regex.test(string)) {
     return null;
-  else
-    return string.replace(regex, '');
+  }
+  return string.replaceAll(regex, '');
 }
 
 module.exports = removeABC;
-
-/*
-console.log(removeABC("This might be a bit hard")) // "This might e  it hrd"
-
-console.log(removeABC("hello world!")) // null
-
-console.log(removeABC("")) // null
-*/
-=======
-function removeABC() {
-  // Your code
-}
-
-module.exports = removeABC;
->>>>>>> 1a7b2e1d9605b833a4fe7ae5c830b7ffd4e05df6
