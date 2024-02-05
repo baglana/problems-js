719f2a42bd607ad24c44d0ea241caa22f22ea3d9
baglana <102351104+baglana@users.noreply.github.com>
2024-02-05 11:24:39 +0600

    Solve exercise3/problem{1, 3}

--- exercise3/problem1/index.js
+++ exercise3/problem1/index.js
@@ -1,5 +1,13 @@
-function concat() {
-  // Your code
+function concat(...arrs) {
+  return [].concat(...arrs);
 }
+
+console.log(concat([1, 2, 3], [4, 5], [6, 7])) // [1, 2, 3, 4, 5, 6, 7]
+
+console.log(concat([1], [2], [3], [4], [5], [6], [7])) // [1, 2, 3, 4, 5, 6, 7]
+
+console.log(concat([1, 2], [3, 4])) // [1, 2, 3, 4]
+
+console.log(concat([4, 4, 4, 4, 4])) // [4, 4, 4, 4, 4]
 
 export default concat;
--- exercise3/problem3/index.js
+++ exercise3/problem3/index.js
@@ -1,5 +1,17 @@
-function pentagonal() {
-  // Your code
+function pentagonal(num) {
+  if (num <= 1) {
+      return 1;
+  }
+
+  return 5 * (num - 1) + pentagonal(num - 1);
 }
+
+console.log(pentagonal(1)) // 1
+
+console.log(pentagonal(2)) // 6
+
+console.log(pentagonal(3)) // 16
+
+console.log(pentagonal(8)) // 141
 
 export default pentagonal;
